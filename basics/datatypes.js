f6def02b051621c0a07a5023a60299ce80252bcf
akhand012 <akhandpratapsinghgaharwar03@gmail.com>
2024-08-28 07:35:21 +0000

    container commit done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -5,3 +5,6 @@ let loginInfo = true
 //large numbers bigint example: 2 to the power 69
 //string= ""
 // boolean = true/false
+console.log(typeof'akhand'); //how does it works in both the ways??//
+console.log(typeof undefined);
+console.log(typeof null);
