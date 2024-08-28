3261caa008c590ada0734df7c286bdbac9ab016a
akhand012 <akhandpratapsinghgaharwar03@gmail.com>
2024-08-28 08:28:24 +0000

    container commit done

---
+++ basics/conversion_operation.js
@@ -0,0 +1,14 @@
+//let score = "33abc"//
+//let score = 33//
+//let score = 33//
+//let score = null//
+//let score = undefined//
+//let score = true//
+let score = false
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//NaN means not a number
