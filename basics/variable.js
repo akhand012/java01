7eaf2e44072de0ff4c07e7c291cbca7f45318343
akhand012 <akhandpratapsinghgaharwar03@gmail.com>
2024-08-28 06:00:46 +0000

    container commit done

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 54321
+let accountEmail = "hello@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 const cant be changed
+console.log(accountId) //for single variable
+
+console.table([accountEmail,accountId,accountPassword])
